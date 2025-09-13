fd3a6a0603b6928d10f7e966fb1d71cab87c345a
YogeshVenugopalR <yogesh.r@zipsar.com>
2025-09-13 19:35:21 +0530

    Add AboutCard component for displaying information with icon, title, and description

--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import { ChartColumnIncreasing, Component, Lightbulb, PenTool } from "lucide-react";
+import React from "react";
+import AboutCard from "../Components/Card/AboutCard";
 
 const About = () => {
   return (
-    <section id='About' className='h-screen flex justify-center items-center'>
-      About
+    <section id="About" className="h-screen flex justify-center items-center flex-col">
+      <div>
+        <h1 className="font-bold text-4xl">Our <span className="text-orange-400">Vibe..</span></h1>
+      </div>
+      <div className="w-[80%] flex items-center justify-between mx-auto mt-10">
+        <div className="w-[48%]">
+            <img src="/Images/about.png" alt="About Image" className="w-full h-full object-fill" />
+        </div>
+        <div className="w-[48%]">
+          <p className="md:text-sm">
+            We are a passionate team of digital marketing professionals who
+            believe that great brands are built at the intersection of
+            creativity, strategy, and technology. Our journey started with a
+            simple vision: to help businesses stand out in the crowded digital
+            landscape by combining innovative ideas with measurable results.<br/><br/> At
+            our core, we are more than just marketers — we are storytellers,
+            designers, analysts, and problem solvers. Every campaign we create
+            is tailored to reflect your brand’s unique identity, while staying
+            aligned with your long-term business goals. We understand that
+            digital marketing is not just about ads or posts, but about creating
+            meaningful connections with your audience.<br/><br/> Our strength lies in
+            blending creativity with data-driven decision-making. From building
+            visually striking campaigns to optimizing them for performance, we
+            ensure that every step delivers value. Whether it’s growing your
+            online presence, generating leads, or increasing engagement, we
+            focus on strategies that bring real, lasting results.<br/><br/> Above all, we
+            believe in transparency, collaboration, and trust. When you work
+            with us, you gain a partner who is as invested in your success as
+            you are. Together, we’ll transform ideas into impact and make your
+            brand truly unforgettable.
+          </p>
+        </div>
+      </div>
+      <div className="mt-10 w-[80%] mx-auto grid grid-cols-1 md:grid-cols-4 gap-6">
+        <AboutCard 
+        title="Creative & Bold Strategies"
+        description="Tailored strategies that help your brand stand out and grow with impact."
+        icon={<PenTool />}
+        />
+         <AboutCard 
+        title="Designs That Stand Out"
+        description="Compelling visuals that reflect your identity and engage your audience."
+        icon={<Component />}
+        />
+         <AboutCard 
+        title="Data-Driven Decisions"
+        description="Campaigns optimized through insights and measurable performance metrics."
+        icon={<ChartColumnIncreasing />}
+        />
+         <AboutCard 
+        title="Innovation at Every Step"
+        description="Fresh ideas and modern tools to keep your brand ahead of the curve."
+        icon={<Lightbulb />}
+        />
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -7,13 +7,13 @@ const Hero = () => {
       className="h-[calc(100vh-80px)] flex justify-center items-center flex-col relative"
     >
       <div className="flex flex-col items-center justify-center gap-1">
-        <h1 className="text-4xl font-bold">We Craft Bold Digital Stories</h1>
+        <h1 className="text-4xl font-bold">We Craft Bold <span className="text-orange-400">Digital</span> Stories</h1>
         <h1 className="text-4xl font-bold">That Sparkle, Engage and Convert</h1>
       </div>
       <p className="text-gray-900/60 my-2">"From strategy to storytelling, we blend creativity with performance marketing to help brands shine in the digital playground."</p>
       <div className="flex gap-4 mt-4">
-        <button className="cursor-pointer font-semibold py-2 px-3 bg-orange-400 rounded-[40px]">Let's Create Magic</button>
-        <button className="cursor-pointer font-semibold border-2 px-3 py-2 border-orange-400 rounded-[40px]">See Our Work</button>
+        <button className="cursor-pointer font-semibold py-2 px-4 text-white bg-orange-400 rounded-[40px]">Let's Create Magic</button>
+        <button className="cursor-pointer font-semibold border-2 px-4 py-2 text-orange-400 border-orange-400 rounded-[40px]">See Our Work</button>
       </div>
     </section>
   );
